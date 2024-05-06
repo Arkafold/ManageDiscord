ff952ed336a40c91bc3f450e6fbb5c47f3d0ac58
René Bus <rene.p.bus@gmail.com>
2024-05-06 22:06:22 +0200

    Added License, added check to payload, fixed encoding issue for module manifest

---
+++ Private/ps1/Approve-DiscordPayload.ps1
@@ -0,0 +1,29 @@
+﻿<#
+.SYNOPSIS
+    Tests if the Payload is a valid Discord Webhook Payload.
+.LINK
+    https://github.com/Arkafold/ManageDiscord
+.EXAMPLE
+    Approve-DiscordPayload -Payload $yourPayload
+    Returns $true on succes, throws an error on failure.
+#>
+
+function Approve-DiscordPayload {
+
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([bool])]
+
+    param (
+        [Parameter(Mandatory = $true)]
+        [PSCustomObject] $Payload
+    )
+
+    #Check if the payload has either the content or the embeds required property
+    if (($payload.PSObject.Properties.name -match "content") -or ($payload.PSObject.Properties.name -match "embeds")) {
+        return $true
+    } else {
+        throw "The provided payload did not contain on of the required components. Please add either content or embeds or both."
+    }
+
+}
--- Public/ps1/Send-DiscordMessage.ps1
+++ Public/ps1/Send-DiscordMessage.ps1
@@ -4,7 +4,7 @@
 .DESCRIPTION
     This function takes a PSCustomObject Payload, converts it to JSON and uses the provided Url to send a message to a Discord Webhook.
 .PARAMETER Payload
-    Mandatory Payload PSCustomObject created by Set-DiscordMessage.
+    Mandatory Payload PSCustomObject created by New-DiscordMessage.
 .PARAMETER WebhookUrl
     Mandatory URL of the Webhook of the Discord channel you want your message to be send to.
 .PARAMETER LogJson
@@ -13,7 +13,7 @@
     https://github.com/Arkafold/ManageDiscord
 .EXAMPLE
     Send-DiscordMessage -Payload $Payload -WebhookUrl "https://discord.com/api/webhooks/etc/etc"
-    Takes Payload created with Set-DiscordMessage and send it to the specified Discord Webhook
+    Takes Payload created with New-DiscordMessage and send it to the specified Discord Webhook
 #>
 
 function Send-DiscordMessage {
@@ -36,23 +36,28 @@ function Send-DiscordMessage {
         $Payload | ConvertTo-Json -Depth 100 | Out-File ".\logs\Payload.json" -Force
     }
 
-    try {
-        $response = Invoke-RestMethod -Uri $WebhookUrl -Method Post -Body ($Payload | ConvertTo-Json -Depth 100) -ContentType 'Application/Json'
-        # Assuming the response is a dictionary, you can access its properties
-        $statusCode = $response.StatusCode
-        $message = $response.Message
-        Write-Verbose "Status Code: $statusCode"
-        Write-Verbose "Message: $message"
-    } catch {
-        # Dig into the exception to get the response details
-        if ($_.Exception.Response) {
-            $statusCode = $_.Exception.Response.StatusCode.value__
-            $statusDescription = $_.Exception.Response.StatusDescription
+    #Check if the payload is valid before trying to send it.
+    if (Approve-DiscordPayload -Payload $Payload) {
+        try {
+            $response = Invoke-RestMethod -Uri $WebhookUrl -Method Post -Body ($Payload | ConvertTo-Json -Depth 100) -ContentType 'Application/Json'
+            # Assuming the response is a dictionary, you can access its properties
+            $statusCode = $response.StatusCode
+            $message = $response.Message
             Write-Verbose "Status Code: $statusCode"
-            Write-Verbose "Status Description: $statusDescription"
-        } else {
-            Write-Verbose "Error: $_"
+            Write-Verbose "Message: $message"
+        } catch {
+            # Dig into the exception to get the response details
+            if ($_.Exception.Response) {
+                $statusCode = $_.Exception.Response.StatusCode.value__
+                $statusDescription = $_.Exception.Response.StatusDescription
+                Write-Verbose "Status Code: $statusCode"
+                Write-Verbose "Status Description: $statusDescription"
+            } else {
+                Write-Verbose "Error: $_"
+            }
         }
+    } else {
+        Write-Verbose "Error: $_"
     }
 
 }
